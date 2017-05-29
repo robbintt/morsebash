ef50476ad796deb6e2e2b33bb3c73cd4f52abd94
Trent Robbins <robbintt@gmail.com>
2017-05-28 20:47:38 -0700

    Broke keyup event into a function that can be expanded, reduced debounce for testing.

--- demo.termios.c
+++ demo.termios.c
@@ -6,8 +6,9 @@
 
 struct termios info;
 
+int counter = 0;
 int keydown = 0;
-struct timeval t1, t2, t_debounce;
+struct timeval t1, t2;
 
 /* termios - from: https://stackoverflow.com/a/21101030
  * see: `man termios` */
@@ -37,15 +38,19 @@ void teardown_termios()
   tcsetattr(0, TCSANOW, &info);
 }
 
+double elapsedTime;
 // compute and print the elapsed time in millisec
 double yield_time()
 {
-  double elapsedTime;
   elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
   elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
-  printf("%f ms.\n", elapsedTime);
 
   return elapsedTime;
+}
+void keyup_event()
+{
+  printf("Event %i, at %f ms.\n", counter, elapsedTime);
+  counter++;
 }
 
 void termios_demo()
@@ -53,34 +58,69 @@ void termios_demo()
 
   int ch;
   while((ch = getchar()) != 27 /* ascii ESC */) {
-		if (ch < 0) { if (ferror(stdin)) { /* there was an error... */ }
+
+    // manage errors and debounce
+		if (ch < 0) { 
+
+      // deal with stdin error
+      if (ferror(stdin)) { /* there was an error... */ }
 
       // i want to test debounce here and yield the time if debounce state is false
       // same code as if you hit another key...
       if (keydown == 1) {
         gettimeofday(&t2, NULL);
-        gettimeofday(&t_debounce, NULL);
 
+        // this is a 2.5 second debounce timer on the error
         if (yield_time() > 2500) {
           keydown = 0; /* set keyup */
+          keyup_event();
         }
       }
 
 			clearerr(stdin);
 			/* do other stuff */
-    } else if (ch == 'f') {
-      keydown = 1; /* set keydown */
-      gettimeofday(&t1, NULL);
+    } 
+    // manage 'f' down case
+    else if (ch == 'f') {
       printf("%c\n", ch);
-		} else {
+      if (keydown == 1) {
+        gettimeofday(&t2, NULL);
+
+        // give some debounce on the f key
+        if (yield_time() > 20) {
+          // the state is actually no longer keyup... we can get rid of this concept
+          keydown = 0; /* set keyup */
+
+          // this is our event
+          keyup_event();
+
+          // reset the f keydown event time
+          gettimeofday(&t1, NULL);
+          keydown = 1; /* set keyup */
+        }
+        else {
+          printf("debounce event: f.\n");
+        }
+      }
+      else {
+        keydown = 1; /* set keydown */
+        gettimeofday(&t1, NULL);
+      } 
+    }
+    // what if there is a key that is not the f
+    else {
 			/* some key OTHER than ESC was hit, do something about it? */
       
+      /*
       if (keydown == 1) {
-        keydown = 0; /* set keyup */
+        keydown = 0; // set keyup 
         gettimeofday(&t2, NULL);
+        printf("%f ms.\n", elapsedTime);
 
         yield_time();
       }
+      */
+
       // give difftime of keydown and keyup
       // how will this work given VMIN is always returning an error... 
       // i guess in error we will reset the flag too
